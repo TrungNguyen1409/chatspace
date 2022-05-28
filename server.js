4f51759bdffffd99ad5c5ba640918930bc655ea3
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-05-28 11:27:06 +0200

    script to start server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 // entry point to everything: backend
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
