4badf8e40a6811aa7c9981d6c5615157d9a06f25
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-05-28 11:50:56 +0200

    adding socket.io to server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // entry point to everything: backend
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connect
+io.on('connection',socket =>{
+    console.log('New WS Connection...');
+});
+
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
